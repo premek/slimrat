c796bc99f88851caaa4616c0fa4abfa2c296b64d
premysl.vyhnal <premysl.vyhnal@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-05-17 09:51:42 +0000

    svn updated to 0.9.4

---
+++ trunk/Toolbox.pm
@@ -0,0 +1,37 @@
+# SlimRat 
+# Přemek Vyhnal <premysl.vyhnal gmail com> 2009 
+# public domain
+
+package Toolbox;
+
+use Exporter;
+@ISA=qw(Exporter);
+@EXPORT=qw(dwait ptime $useragent);
+
+use strict;
+use warnings;
+
+our $useragent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.4) Gecko/20030624";
+
+sub dwait{
+	my ($wait, $rem, $sec, $min);
+	$wait = $rem = shift or return;
+	$|++; # unbuffered output;
+	($sec,$min) = localtime($wait);
+	printf(&ptime."Waiting %02d:%02d\n",$min,$sec);
+	while ($rem){
+		($sec,$min) = localtime($rem);
+		printf "\r%02d:%02d remaining ",$min,$sec;
+		sleep 1;
+		$rem--;
+	}
+	print "\r".&ptime."Continuing\n";
+}
+
+sub ptime {
+	my ($sec,$min,$hour) = localtime;
+	sprintf "[%02d:%02d:%02d] ",$hour,$min,$sec;
+}
+
+
+1;
