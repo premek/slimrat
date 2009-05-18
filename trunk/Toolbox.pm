cb70bf3a432fd97ba0dc18ec791786d484bc9a03
tim.besard <tim.besard@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-05-18 14:45:34 +0000

    Refactored slimrat-cli and main subpackages a bit.

--- trunk/Toolbox.pm
+++ trunk/Toolbox.pm
@@ -2,17 +2,29 @@
 # Přemek Vyhnal <premysl.vyhnal gmail com> 2009 
 # public domain
 
+#
+# Configuration
+#
+
 package Toolbox;
 
 use Exporter;
 @ISA=qw(Exporter);
 @EXPORT=qw(dwait ptime $useragent);
 
+# Write nicely
 use strict;
 use warnings;
 
+# Fake a user agent
 our $useragent = "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.4) Gecko/20030624";
 
+
+#
+# Routines
+#
+
+# Wait a while
 sub dwait{
 	my ($wait, $rem, $sec, $min);
 	$wait = $rem = shift or return;
@@ -22,10 +34,10 @@ sub dwait{
 	sleep ($rem);
 }
 
+# Generate a timestamp
 sub ptime {
 	my ($sec,$min,$hour) = localtime;
 	sprintf "[%02d:%02d:%02d] ",$hour,$min,$sec;
 }
-
 
 1;
