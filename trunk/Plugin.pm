a77dbaa01f6d621202a8a89a1920a30aca4041fc
tim.besard <tim.besard@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-05-20 10:19:32 +0000

    MIT licensing.

--- trunk/Plugin.pm
+++ trunk/Plugin.pm
@@ -1,6 +1,37 @@
-# SlimRat 
-# Přemek Vyhnal <premysl.vyhnal gmail com> 2008 
-# public domain
+#!/usr/bin/env perl
+#
+# slimrat - plugin infrastructure
+#
+# Copyright (c) 2008-2009 Přemek Vyhnal
+#
+# This file is part of slimrat, an open-source Perl scripted
+# command line and GUI utility for downloading files from
+# several download providers.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Authors:
+#    Přemek Vyhnal <premysl.vyhnal gmail com> 
+#
 
 #
 # Configuration
@@ -53,4 +84,5 @@ sub get_name {
 my @pluginfiles = glob "$root/plugins/*.pm";
 do $_ || do{system("perl -c $_"); die "\nPlugin $_ failed to load!\n\n"} foreach @pluginfiles;
 print "Loaded plugins: "; my $oc = $,; $,=", "; print values %plugins; $, = $oc; print "\n";
-scalar @pluginfiles; # no plugins: return false
+
+scalar @pluginfiles; # Returns 0 (= failure to load) if no plugins present
--- trunk/plugins/FastLoad.pm
+++ trunk/plugins/FastLoad.pm
@@ -1,7 +1,37 @@
-# SlimRat 
-# Tomasz [TiMoR] Gągor <timor o2 pl> 2008
-# based on Přemek Vyhnal Rapidshare plugin  
-# public domain
+#!/usr/bin/env perl
+#
+# slimrat - FastLoad plugin
+#
+# Copyright (c) 2008 Tomasz Gągor
+#
+# This file is part of slimrat, an open-source Perl scripted
+# command line and GUI utility for downloading files from
+# several download providers.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Authors:
+#    Tomasz Gągor <timor o2 pl>
+#
 
 package FastLoad;
 use Term::ANSIColor qw(:constants);
@@ -43,3 +73,5 @@ sub download {
 }
 
 Plugin::register(__PACKAGE__,"^[^/]+//(?:www.)?fast-load.net");
+
+1;
