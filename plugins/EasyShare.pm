a5ff51bd835f35da2276f46eaaadca1d7df491f0
tim.besard <tim.besard@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-05-20 10:19:32 +0000

    MIT licensing.

--- plugins/EasyShare.pm
+++ plugins/EasyShare.pm
@@ -1,6 +1,37 @@
-# SlimRat 
-# Tim Besard <tim.besard gmail com> 2009 
-# public domain
+#!/usr/bin/env perl
+#
+# slimrat - Easy-Share plugin
+#
+# Copyright (c) 2009 Tim Besard
+#
+# This file is part of slimrat, an open-source Perl scripted
+# command line and GUI utility for downloading files from
+# several download providers.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Authors:
+#    Tim Besard <tim-dot-besard-at-gmail-dot-com>
+#
 
 package EasyShare;
 use Toolbox;
@@ -91,3 +122,4 @@ sub download {
 
 Plugin::register(__PACKAGE__,"^([^:/]+://)?([^.]+\.)?easy-share.com");
 
+1;
--- plugins/ShareBase.pm
+++ plugins/ShareBase.pm
@@ -1,9 +1,36 @@
 #!/usr/bin/env perl
 #
-# SlimRat plugin for ShareBase v0.2
-# Yunnan www.yunnan.tk 2009.04
-# public domain
-# partially based on other plugins
+# slimrat - ShareBase plugin
+#
+# Copyright (c) 2009 Yunnan
+#
+# This file is part of slimrat, an open-source Perl scripted
+# command line and GUI utility for downloading files from
+# several download providers.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Authors:
+#    Yunnan <www.yunnan.tk>
 #
 
 package ShareBase;
@@ -65,3 +92,5 @@ sub download {
 }
 
 Plugin::register(__PACKAGE__,"^[^/]+//(?:www.)?sharebase.to");
+
+1;
