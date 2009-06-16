73bd0a6e33c76544d1dae3807c52e16402ca42bf
premysl.vyhnal <premysl.vyhnal@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-06-16 20:07:39 +0000

    added UploadedTo

--- plugins/UploadedTo.pm
+++ plugins/UploadedTo.pm
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 #
-# slimrat - direct downloading plugin
+# slimrat - UploadedTo plugin
 #
-# Copyright (c) 2008 Přemek Vyhnal
+# Copyright (c) 2009 Přemek Vyhnal
 #
 # This file is part of slimrat, an open-source Perl scripted
 # command line and GUI utility for downloading files from
@@ -30,32 +30,63 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 # Authors:
-#    Přemek Vyhnal <premysl.vyhnal gmail com>
+#    Přemek Vyhnal
 #
 
 # Package name
-package Direct;
+package UploadedTo;
 
 # Modules
-use Toolbox;
 use Log;
-use LWP::UserAgent;
+use Toolbox;
+use WWW::Mechanize;
 
 # Write nicely
 use strict;
 use warnings;
 
-my $ua = LWP::UserAgent->new;
-$ua->agent($useragent);
+my $mech = WWW::Mechanize->new('agent'=>$useragent);
 
+# return
+#   1: ok
+#  -1: dead
+#   0: don't know
 sub check {
-	return 1 if ($ua->head(shift)->is_success);
-	return -1;
+	my $res = $mech->get(shift);
+	if ($res->is_success) {
+		if ($res->decoded_content =~ m#File doesn't exist#) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+	return 0;
 }
 
 sub download {
-	warning("no plugin for this site, downloading using 'Direct' plugin");
-	return shift;
+	my $file = shift;
+
+	my $res = $mech->get($file);
+	return error("plugin failure (", $res->status_line, ")") unless ($res->is_success);
+	
+	$_ = $res->content."\n";
+
+
+	if(my($minutes) = m#Or wait (\d+) minutes!#) { error("Your Free-Traffic is exceeded, wait $minutes minutes."); return 0; }
+
+	# Extract url
+	my ($download) = m#<form name="download_form" method="post" action="(.+?)">#;
+	if (!$download) { error("plugin failure (could not find url)"); return 0; }
+
+	# Extract filename
+	my ($fname) = m/<title>(.+?) \.\.\./s;
+
+	# Generate the download URL
+	$download .= " \" --post-data \"download_submit=Free Download\"";
+	if($fname) {$download .= " -O \"".$fname;} # works like wget http://... -O - > fname (overwrites the file!!!)
+	return $download;
 }
+
+Plugin::register(__PACKAGE__,"^[^/]+//(uploaded.to/file|ul.to)/");
 
 1;
