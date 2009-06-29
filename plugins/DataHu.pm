662ea875df8384048296851ee482bf285bdb0cdc
tim.besard <tim.besard@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-06-29 11:25:07 +0000

    DataHu plugin.

--- plugins/DataHu.pm
+++ plugins/DataHu.pm
@@ -56,7 +56,6 @@ use warnings;
 
 # Constructor
 sub new {
-	return error("plugin not ported yet");
 	my $self  = {};
 	$self->{URL} = $_[1];
 	
@@ -69,6 +68,37 @@ sub new {
 # Plugin name
 sub get_name {
 	return "DataHu";
+}
+
+# Filename
+sub get_filename {
+	my $self = shift;
+	
+	my $res = $self->{MECH}->get($self->{URL});
+	if ($res->is_success) {
+		if ($res->decoded_content =~ m/<div class="download_filename">\s+([^<]+?)\s+<\/div>/s) {
+			return $1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
+}
+
+# Filesize
+sub get_filesize {
+	my $self = shift;
+	
+	my $res = $self->{MECH}->get($self->{URL});
+	if ($res->is_success) {
+		if ($res->decoded_content =~ m/f.jlm.ret:\s+(.+)/) {
+			print "yay\n";
+			return $1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
 }
 
 # Check if the link is alive
