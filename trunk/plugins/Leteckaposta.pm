d83b1877d4633b499bb992ab54500cf1ee5619e6
tim.besard <tim.besard@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-06-29 11:51:18 +0000

    Leteckaposta plugin.

--- trunk/plugins/Leteckaposta.pm
+++ trunk/plugins/Leteckaposta.pm
@@ -56,7 +56,6 @@ use warnings;
 
 # Constructor
 sub new {
-	return error("plugin not ported yet");
 	my $self  = {};
 	$self->{URL} = $_[1];
 	
@@ -69,6 +68,36 @@ sub new {
 # Plugin name
 sub get_name {
 	return "Leteckaposta";
+}
+
+# Filename
+sub get_filename {
+	my $self = shift;
+	
+	my $res = $self->{MECH}->get($self->{URL});
+	if ($res->is_success) {
+		if ($res->decoded_content =~ m/<a[^>]*class='download-link'>([^<]+)<\/a>/) {
+			return $1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
+}
+
+# Filesize
+sub get_filesize {
+	my $self = shift;
+	
+	my $res = $self->{MECH}->get($self->{URL});
+	if ($res->is_success) {
+		if ($res->decoded_content =~ m/Velikost souboru: ([^<]+)<\/p>/) {
+			return $1;
+		} else {
+			return 0;
+		}
+	}
+	return 0;
 }
 
 # Check if the link is alive
