759a7daf503385f058fed613d74989ac7f82e9e9
premysl.vyhnal <premysl.vyhnal@3daedaa2-d654-0410-85d5-9f867be762b4>
2009-07-27 20:45:06 +0000

    How to download more than one file from one link? Example: Depositfiles folder.

---
+++ src/plugins/DepositFilesFolder.pm
@@ -0,0 +1,111 @@
+# slimrat - DepositFiles plugin
+#
+# Copyright (c) 2008 Přemek Vyhnal
+# Copyright (c) 2009 Tim Besard
+#
+# This file is part of slimrat, an open-source Perl scripted
+# command line and GUI utility for downloading files from
+# several download providers.
+# 
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Authors:
+#    Přemek Vyhnal <premysl.vyhnal gmail com> 
+#    Tim Besard <tim-dot-besard-at-gmail-dot-com>
+#
+
+#
+# Configuration
+#
+
+# Package name
+package DepositFilesFolder;
+
+# Packages
+use WWW::Mechanize;
+
+# Custom packages
+use Log;
+use Toolbox;
+use Configuration;
+
+# Write nicely
+use strict;
+use warnings;
+
+
+#
+# Routines
+#
+
+# Constructor
+sub new {
+	my $self  = {};
+	$self->{CONF} = $_[1];
+	$self->{URL} = $_[2];
+	
+	$self->{MECH} = WWW::Mechanize->new(agent=>$useragent);
+
+	# Fetch the language switch page which gives us a "lang_current=en" cookie
+	$self->{MECH}->get('http://depositfiles.com/en/switch_lang.php?lang=en');
+	
+	bless($self);
+	return $self;
+}
+
+# Plugin name
+sub get_name {
+	return "DepositFiles Folder";
+}
+
+# Filename
+sub get_filename { return 0; # not implemented
+}
+
+# Filesize
+sub get_filesize { return 0; # not implemented
+}
+
+# Check if the link is alive
+sub check { return 1; # not implemented
+}
+
+# Download data
+sub get_data {
+	my $self = shift;
+	my $data_processor = shift;
+	
+	my $res = $self->{MECH}->get($self->{URL});
+	return error("plugin failure (", $res->status_line, ")") unless ($res->is_success);
+	dump_add($self->{MECH}->content());
+	
+	$_ = $self->{MECH}->content();
+
+
+	while(m#<a href="(http://depositfiles\.com/files/.+?)" title=".+?" target="_blank">.+?</a>#g){
+		print "--> $1\n";
+	}
+}
+
+Plugin::register(__PACKAGE__,"^[^/]+//(?:www.)?depositfiles.com/(?:[a-z]{2}/)?folders");
+
+1;
